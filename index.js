5238ef2026175dd4e488fe94bab443d9dfa9dd18
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-10-22 08:13:14 +0200

    changed let to const

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let print = require("./stars");
-let stars = print.stars(process.argv[3]);
+const print = require("./stars");
+const stars = print.stars(process.argv[3]);
 const string = process.argv[2] || "hi";
 const arr = [stars, string, stars];
 arr.forEach(x => console.log(x))
